29a825cbe32ed944737a8dd731c5fd058f8a104f
Alex Roth <jonesroth7@gmail.com>
2019-11-20 21:54:29 -0700

    Add deck class

--- src/Deck.js
+++ src/Deck.js
@@ -1,7 +1,12 @@
 class Deck {
-  constructor() {
-
+  constructor(cards) {
+    this.cards = cards;
+    this.cardCount = 0
   };
+
+  countCards() {
+    return this.cardCount = this.cards.length;
+  }
 };
 
 module.exports = Deck;
